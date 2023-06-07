f80874cf7f298df5824be1e409c11deb3468c529
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-06-07 12:31:27 +0500

    Updata

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('/api/login', { email, password });
+            console.log(response.data); // Handle the response data as needed
+        } catch (error) {
+            setError(error.response.data.message);
+        }
+    };
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleLogin}>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email"
+                />
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                />
+                <button type="submit">Login</button>
+            </form>
+            {error && <p>{error}</p>}
+        </div>
+    );
+}
