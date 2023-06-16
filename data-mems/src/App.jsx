32116273d340ffbb41370b9a145d26949bbf25d5
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-06-16 17:07:28 +0500

    update

--- data-mems/src/App.jsx
+++ data-mems/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import LoginForm from './components/pages/Login/LoginForm'
 
 function App() {
   return (
     <div className='container'> 
-      
+      <LoginForm/>
     </div>
   )
 }
---
+++ data-mems/src/components/pages/Login/LoginForm.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import './login.css'
+
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: Perform login logic here
+  };
+
+  return (
+    <div style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <form onSubmit={handleSubmit} style={{ backgroundColor: 'white', padding: '20px', borderRadius: '5px' }} className='box form-signin'>
+        <h2 className='form-signin-heading'>Login</h2>
+        <div  className='bigbox'>
+        <div style={{ marginBottom: '20px' }} className='label'> 
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            style={{ marginLeft: '10px' }}
+            className='form-control'
+            placeholder='Email:'
+          />
+        </div>
+        <div style={{ marginBottom: '20px' }} className='label'>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            style={{ marginLeft: '10px' }}
+            className='form-control'
+            placeholder='Password:'
+          />
+        </div>
+        <button type="submit" style={{ backgroundColor: '#00B2FF', color: 'white', border: 'none', borderRadius: '5px' }} className='btn'>
+          Log in
+        </button>          
+        </div>
+
+        <a href="#">Register</a>
+
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
