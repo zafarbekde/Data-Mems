ba00f3fd0e9d077fae29312691cdbd32e0f3435e
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-06-16 22:43:37 +0500

    upgRDE

--- data-mems/src/components/pages/Login/LoginForm.jsx
+++ data-mems/src/components/pages/Login/LoginForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import DataMems from '../../assets/data-mems.png'
 import './login.css'
 
 
@@ -8,40 +9,45 @@ const LoginForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // TODO: Perform login logic here
+
   };
 
   return (
     <div style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
       <form onSubmit={handleSubmit} style={{ backgroundColor: 'white', padding: '20px', borderRadius: '5px' }} className='box form-signin'>
-        <h2 className='form-signin-heading'>Login</h2>
-        <div  className='bigbox'>
-        <div style={{ marginBottom: '20px' }} className='label'> 
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            style={{ marginLeft: '10px' }}
-            className='form-control'
-            placeholder='Email:'
-          />
+        <div className="form-loginn">
+          <img className='form-signin-heading' src={DataMems} alt="" />
         </div>
-        <div style={{ marginBottom: '20px' }} className='label'>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            style={{ marginLeft: '10px' }}
-            className='form-control'
-            placeholder='Password:'
-          />
+        <div className='bigbox'>
+          <div style={{ marginBottom: '20px' }} className='label'>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              style={{ marginLeft: '10px' }}
+              className='form-control'
+              placeholder='Email:'
+            />
+          </div>
+          <div style={{ marginBottom: '20px' }} className='label'>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              style={{ marginLeft: '10px' }}
+              className='form-control'
+              placeholder='Password:'
+            />
+          </div>
+          <div className="middle">
+            <button type="submit" style={{ backgroundColor: '#00B2FF', width: '100%', color: 'white', border: 'none', borderRadius: '5px' }} className='btn'>
+              Log in
+            </button>
+          </div>
+          <div className="register-link">
+            <a href="#">Register</a>
+          </div>
         </div>
-        <button type="submit" style={{ backgroundColor: '#00B2FF', width: '100%', color: 'white', border: 'none', borderRadius: '5px' }} className='btn'>
-          Log in
-        </button>          
-        </div>
-
-        <a href="#">Register</a>
 
       </form>
     </div>
