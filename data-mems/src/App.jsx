7d17ff071cf1997debdaaaf966615699c566f523
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-06-18 00:11:39 +0500

    update

--- data-mems/src/App.jsx
+++ data-mems/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import LoginForm from './components/pages/Login/LoginForm'
 import Register from './components/pages/Register/Register'
+import Home from './components/pages/Home/Home'
 
 function App() {
   return (
     <div className='container'> 
       {/* <LoginForm/> */}
-      <Register/>
+      {/* <Register/> */}
+      <Home/>
     </div>
   )
 }
