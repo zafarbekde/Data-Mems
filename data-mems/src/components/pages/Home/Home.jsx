68630dd1d6ecab722b99535c1f6e78f3aa200902
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-06-23 12:39:22 +0500

    Upload

--- data-mems/src/components/pages/Home/Home.jsx
+++ data-mems/src/components/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './home.css';
-import './button.css';
+
 
 function Home() {
     return (
@@ -8,11 +8,13 @@ function Home() {
             <nav className="menu">
                 <ul>
                     <div className="left-logo"><li><a href="/">Memes</a></li></div>
-                    <div className="right-menu"><li><button className='right-menu-btn'>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"></path></svg>
-                        <span>Upload</span>
-                    </button></li>
-                        <li><a href="/profile">Profile</a></li></div>
+                    <div className="right-menu">
+                        <li><button className='upload-btn' id='upload'> + Upload</button></li>
+                            
+                            <li className='menu-profile'><a href="/profile"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg></a></li>
+                    </div>
                 </ul>
             </nav>
 
