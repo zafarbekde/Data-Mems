d61c023c714509bf87848edd7f863f0238e614ff
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-06-26 12:39:18 +0500

    upkoad

--- data-mems/src/components/pages/Home/Home.jsx
+++ data-mems/src/components/pages/Home/Home.jsx
@@ -1,52 +1,136 @@
-import React from 'react';
-import { useQuery } from 'react-query';
+import React, { useState, useEffect, useRef } from 'react';
 import './home.css';
 
 function Home() {
-  const selectedCategory = 'memes'; // Set your desired category here
+  const [images, setImages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [selectedCategory, setSelectedCategory] = useState('memes');
+  const dropdownRef = useRef(null);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  useEffect(() => {
+    fetchImages();
+  }, [selectedCategory, currentPage]);
 
   const fetchImages = async () => {
-    const response = await fetch(
-      `https://pixabay.com/api/?key=563492ad6f91700001000001a9f2c1745627440f86cd94bd8aa19fed&q=${selectedCategory}&per_page=6&page=1`
-    );
-    const data = await response.json();
-    return data.hits;
+    try {
+      const response = await fetch(
+        `https://pixabay.com/api/?key=33822004-9d83312280bc95290f1335ddd&q=${selectedCategory}&per_page=6&page=${currentPage}`
+      );
+      const data = await response.json();
+      setImages(data.hits);
+      setTotalPages(Math.ceil(data.totalHits / 6));
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  const { data: images, isLoading, isError } = useQuery('images', fetchImages);
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+    setCurrentPage(1);
+  };
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
-  if (isError) {
-    return <div>Error fetching images.</div>;
-  }
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
 
   return (
     <div className="container">
       <nav className="menu">
-        {/* Menu code */}
+        <ul>
+          <div className="left-logo">
+            <li>
+              <a href="/">Memes</a>
+            </li>
+          </div>
+          <div className="right-menu">
+            <li>
+              <button className="upload-btn" id="upload">
+                + Upload
+              </button>
+            </li>
+            <li className="menu-profile">
+              <a href="/profile">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+              </a>
+            </li>
+            <li className="dropdown" ref={dropdownRef}>
+              <button className="dropdown-btn" onClick={toggleDropdown}>
+                Categories
+              </button>
+              {isDropdownOpen && (
+                <ul className="dropdown-menu">
+                  <li>
+                    <button
+                      className={`category-item ${
+                        selectedCategory === 'memes' ? 'active' : ''
+                      }`}
+                      onClick={() => handleCategoryChange('memes')}
+                    >
+                      Memes
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className={`category-item ${
+                        selectedCategory === 'nature' ? 'active' : ''
+                      }`}
+                      onClick={() => handleCategoryChange('nature')}
+                    >
+                      Nature
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className={`category-item ${
+                        selectedCategory === 'animals' ? 'active' : ''
+                      }`}
+                      onClick={() => handleCategoryChange('animals')}
+                    >
+                      Animals
+                    </button>
+                  </li>
+                  {/* Add more categories as needed */}
+                </ul>
+              )}
+            </li>
+          </div>
+        </ul>
       </nav>
 
-      <div className="content-upload">
-        <h2>Welcome!</h2>
-        <div className="image-grid">
-          {images.map((image) => (
-            <img
-              key={image.id}
-              src={image.webformatURL}
-              alt={image.tags}
-              className="image-item"
-            />
-          ))}
-        </div>
-        {/* Pagination code */}
-      </div>
-
-      <div className="profile">
-        <h3>Профиль</h3>
-      </div>
+      {/* Rest of the component code */}
     </div>
   );
 }
