1abdff5ea93d4852f550e59995039c34d8b7415c
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-06-30 09:56:38 +0500

    updATE

--- data-mems/src/components/pages/Upload/Upload.jsx
+++ data-mems/src/components/pages/Upload/Upload.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
-import './upload.css'
+import React, { useState } from 'react';
 
 function Modal({ closeModal }) {
+  const [uploadedImage, setUploadedImage] = useState(null);
+  const [uploadedText, setUploadedText] = useState('');
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    // Perform any necessary validation or processing of the uploaded image
+    setUploadedImage(file);
+  };
+
+  const handleTextChange = (event) => {
+    const text = event.target.value;
+    setUploadedText(text);
+  };
+
+  const handleSubmit = () => {
+    // Handle the submission of the uploaded image and text
+    // You can access the uploaded image via `uploadedImage` state variable
+    // and the uploaded text via `uploadedText` state variable
+    console.log('Uploaded Image:', uploadedImage);
+    console.log('Uploaded Text:', uploadedText);
+
+    // Reset the form after submission
+    setUploadedImage(null);
+    setUploadedText('');
+  };
+
   return (
     <div className="modal">
       <div className="modal-content">
-        {/* Add content for the modal, including the image upload functionality */}
+        <h2>Upload Image and Text</h2>
+        <div className="form-group">
+          <label htmlFor="imageUpload">Upload Image:</label>
+          <input type="file" id="imageUpload" onChange={handleImageUpload} accept="image/*" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="textUpload">Enter Text:</label>
+          <textarea id="textUpload" rows="4" value={uploadedText} onChange={handleTextChange} />
+        </div>
+        <button className="submit-btn" onClick={handleSubmit}>
+          Submit
+        </button>
         <button className="close-btn" onClick={closeModal}>
           Close
         </button>
